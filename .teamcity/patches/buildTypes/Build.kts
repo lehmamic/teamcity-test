f07365cca5243e0ebe5a5c04993025f4ad67e6b0
leh <leh@TeamCity>
2018-10-12 19:16:56 +0000

    TeamCity change in 'Teamcity Test' project: runners of 'Build' build configuration were updated

--- .teamcity/patches/buildTypes/Build.kts
+++ .teamcity/patches/buildTypes/Build.kts
@@ -1,6 +1,7 @@
 package patches.buildTypes
 
 import jetbrains.buildServer.configs.kotlin.v2018_1.*
+import jetbrains.buildServer.configs.kotlin.v2018_1.buildSteps.script
 import jetbrains.buildServer.configs.kotlin.v2018_1.triggers.VcsTrigger
 import jetbrains.buildServer.configs.kotlin.v2018_1.triggers.vcs
 import jetbrains.buildServer.configs.kotlin.v2018_1.ui.*
@@ -11,6 +12,19 @@ To apply the patch, change the buildType with id = 'Build'
 accordingly, and delete the patch script.
 */
 changeBuildType(RelativeId("Build")) {
+    expectSteps {
+    }
+    steps {
+        insert(0) {
+            script {
+                scriptContent = """
+                    wget https://github.com/GitTools/GitVersion/archive/v4.0.0.zip
+                    unzip v4.0.0.zip -d GitVersion
+                """.trimIndent()
+            }
+        }
+    }
+
     triggers {
         val trigger1 = find<VcsTrigger> {
             vcs {
