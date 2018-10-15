5f10ca5d696f244d998123c854db99e8f1fed7f9
leh <leh@TeamCity>
2018-10-15 08:41:48 +0000

    TeamCity change in 'Teamcity Test' project: runners of 'Build' build configuration were updated

--- .teamcity/patches/buildTypes/Build.kts
+++ .teamcity/patches/buildTypes/Build.kts
@@ -18,7 +18,7 @@ changeBuildType(RelativeId("Build")) {
         insert(0) {
             script {
                 scriptContent = """
-                    curl https://github.com/GitTools/GitVersion/archive/v4.0.0.zip --output GitVersion.zip
+                    curl -LO https://github.com/GitTools/GitVersion/archive/v4.0.0.zip --output GitVersion.zip
                     unzip GitVersion.zip -d GitVersion
                 """.trimIndent()
             }
